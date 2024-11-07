2f0a457c4747ffae71e4efa65c9c6aa7bacc10e5
plpaesz <159171613+plpaesz@users.noreply.github.com>
2024-11-07 01:12:47 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,8 @@
+import visao.ClasseProdutoMenu;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ClasseProdutoMenu menuzinho = new ClasseProdutoMenu();
+        menuzinho.ExibirMenu();
+    }
+}
